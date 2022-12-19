6923feb770ba9f34a0c0106934965dd4b4cfcde4
Myelhome <rgty2000@mail.ru>
2022-12-20 00:35:01 +0400

    vectors 2D 3D

---
+++ src/en/opengl/com/entity/Vector2D.java
@@ -0,0 +1,11 @@
+package en.opengl.com.entity;
+
+public class Vector2D {
+    public double x;
+    public double y;
+
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/en/opengl/com/entity/Vector3D.java
@@ -0,0 +1,46 @@
+package en.opengl.com.entity;
+
+public class Vector3D {
+    public double x;
+    public double y;
+    public double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static Vector3D sum(Vector3D a, Vector3D b) {
+        return new Vector3D(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static Vector3D diff(Vector3D a, Vector3D b) {
+        return new Vector3D(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static double dot(Vector3D a, Vector3D b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    public static Vector3D cross(Vector3D a, Vector3D b) {
+        double tmpX = a.y * b.z - a.z * b.y;
+        double tmpY = b.x * a.z - b.z * a.x;
+        double tmpZ = a.x * b.y - a.y * b.x;
+
+        return new Vector3D(tmpX, tmpY, tmpZ);
+    }
+
+    public Vector3D normalize() {
+        double norm = 1.0 / length();
+        double newX = this.x * norm;
+        double newY = this.y * norm;
+        double newZ = this.z * norm;
+
+        return new Vector3D(newX, newY, newZ);
+    }
+
+    public double length() {
+        return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+    }
+}
